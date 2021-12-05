1da2882c3d018f70e9cd302a6ff6d5570ab720f2
Pascal Syma <pascal@syma.dev>
2021-12-05 23:07:15 +0100

    Remove video on ended call
    Signed-off-by: Pascal Syma <pascal@syma.dev>

--- public/script.js
+++ public/script.js
@@ -26,6 +26,9 @@ myPeer.on('open', id => {
             call.on('stream', userVideoStream => {
                 addVideoStream(video, userVideoStream)
             })
+            call.on('close', () => {
+                video.remove()
+            })
         })
 
         socket.on('user-connected', userId => {
